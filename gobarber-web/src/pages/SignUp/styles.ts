414e6de22b84e66cbe98399930a3c8037ddd97cd
Hantonny Korrea <hantonny.20@gmail.com>
2020-11-25 15:16:54 -0400

    1 - Página de cadastro

--- gobarber-web/src/pages/SignUp/styles.ts
+++ gobarber-web/src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { shade } from 'polished'
 
-import signInBackgroundImg from '../../assets/sign-in-background.png'
+import signUpBackgroundImg from '../../assets/sign-up-background.png'
 
 export const Container = styled.div`
     height: 100vh;
@@ -42,9 +42,9 @@ export const Content = styled.div`
     }
 
     > a {
-            color: #FF9000;
+            color: #F4EDE8;
             display: block;
-            margin-top: 24px;
+            margin-top: -24px;
             text-decoration: none;
             transition: color 0.2s;
 
@@ -55,12 +55,12 @@ export const Content = styled.div`
                 margin-right: 16px;
             }
             &:hover {
-                color: ${shade(0.2, '#FF9000')};
+                color: ${shade(0.2, '#F4EDE8')};
             }
     }
 `;
 export const Background = styled.div`
     flex: 1;
-    background: url(${signInBackgroundImg}) no-repeat center;
+    background: url(${signUpBackgroundImg}) no-repeat center;
     background-size: cover;
 `;
