bb8e6b65123340e6e9ba571d5101cd5d94554610
Hantonny Korrea <hantonny.20@gmail.com>
2020-11-25 14:36:54 -0400

    1 - Isolando componentes

---
+++ gobarber-web/src/components/Input/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+            background: #232129;
+            border-radius: 10px;
+            border: 2px solid #232129;
+            padding: 16px;
+            color: #666360;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            & + div {
+                margin-top: 8px;
+            }
+        input {
+            flex: 1;
+            border: 0;
+            background: transparent;
+            color: #F4EDE8;
+            &::placeholder {
+                color: #666360;
+            }
+        
+        }
+        svg {
+            margin-right: 16px;
+        }
+`;
--- gobarber-web/src/pages/Signin/index.tsx
+++ gobarber-web/src/pages/Signin/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
-
+import Input from '../../components/Input';
+import Button from '../../components/Button'
 import { Container, Content, Background } from './styles';
+
+
 
 const SignIn: React.FC = () => (
     <Container>
@@ -11,11 +14,11 @@ const SignIn: React.FC = () => (
             <img src={logoImg} alt="GoBarber" />
             <form>
                 <h1>Faça seu logon</h1>
-                <input placeholder="E-mail" />
-                <input type="password" placeholder="Senha" />
-                <button type="submit">
+                <Input icon={FiMail} name="email" placeholder="E-mail" />
+                <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+                <Button type="submit">
                     Entrar
-                </button>
+                </Button>
                 <a href="forgot">Esqueci minha senha</a>
             </form>
             <a href="dss"><FiLogIn />Criar conta</a>
