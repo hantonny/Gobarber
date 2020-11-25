327a9d2eb478e286edad03d4e92e85735a77cc2e
Hantonny Korrea <hantonny.20@gmail.com>
2020-11-25 15:40:17 -0400

    1 - Utilizando Unform

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser} from 'react-icons/fi'
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -8,24 +9,28 @@ import { Container, Content, Background } from './styles';
 
 
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
-            <form>
-                <h1>Faça seu cadastro</h1>
-                <Input icon={FiUser} name="name" placeholder="Nome" />
-                <Input icon={FiMail} name="email" placeholder="E-mail" />
-                <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
-                <Button type="submit">
-                    Cadastrar
-                </Button>
-            </form>
-            <a href="dss"><FiArrowLeft />Voltar para Logon</a>
-        </Content>
-        
-    </Container>
-)
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object): void {
+        console.log(data);
+    }
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input icon={FiUser} name="name" placeholder="Nome" />
+                    <Input icon={FiMail} name="email" placeholder="E-mail" />
+                    <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+                    <Button type="submit">
+                        Cadastrar
+                    </Button>
+                </Form>
+                <a href="dss"><FiArrowLeft />Voltar para Logon</a>
+            </Content>
+        </Container>
+    );
+}
 
 export default SignUp;
