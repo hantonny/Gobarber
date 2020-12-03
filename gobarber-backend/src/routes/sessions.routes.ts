c4e64d890d7eafd59ee3b0053d74be84a33a56ca
Hantonny Korrea <hantonny.20@gmail.com>
2020-12-03 18:53:00 -0400

    1 - Corrigindo erro storage

--- gobarber-backend/src/routes/sessions.routes.ts
+++ gobarber-backend/src/routes/sessions.routes.ts
@@ -5,24 +5,24 @@ import AuthenticationUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticationUserService();
+  const authenticateUser = new AuthenticationUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json({ userWithoutPassword, token });
+  return response.json({ user: userWithoutPassword, token });
 });
 
 export default sessionsRouter;
