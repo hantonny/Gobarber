378e79584ce9318ce27b1d73e59c0ec00b39c92a
Hantonny Korrea <hantonny.20@gmail.com>
2020-12-03 19:01:04 -0400

    1 - Criando hook useAuth

--- gobarber-web/src/context/AuthContext.tsx
+++ gobarber-web/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 
 import api from '../services/api';
 
@@ -53,4 +53,12 @@ const AuthProvider: React.FC = ({ children }) => {
   );
 };
 
-export { AuthContext, AuthProvider };
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
+export { AuthProvider, useAuth };
