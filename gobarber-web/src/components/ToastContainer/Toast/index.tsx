9bd2c5868ef5cfe717e04f4387ec533b1331ead6
Hantonny Korrea <hantonny.20@gmail.com>
2020-12-05 23:14:32 -0400

    :sparkles: - Animando Toasts

--- gobarber-web/src/components/ToastContainer/Toast/index.tsx
+++ gobarber-web/src/components/ToastContainer/Toast/index.tsx
@@ -12,13 +12,14 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 const icons = {
   info: <FiInfo size={24} />,
   error: <FiAlertCircle size={24} />,
   success: <FiCheckCircle size={24} />,
 };
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -31,7 +32,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      style={style}
+      type={message.type}
+      hasDescription={!!message.description}
+    >
       {icons[message.type || 'info']}
       <div>
         <strong>{message.title}</strong>
