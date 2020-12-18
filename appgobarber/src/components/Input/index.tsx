2d6053ddf7c66d6cf455ab90f660346f9dd60ae6
Hantonny Korrea <hantonny.20@gmail.com>
2020-12-17 22:02:06 -0400

    :sparkles: - Usabilidade em formulários

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useImperativeHandle, forwardRef} from 'react';
 import {TextInputProps} from 'react-native';
 import {useField} from '@unform/core';
 
@@ -10,12 +10,26 @@ interface InputValueReference {
   value: string;
 }
 
+interface InputRef {
+  focus(): void;
+}
+
 import {Container, TextInput, Icon} from './styles';
 
-const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
+  {name, icon, ...rest},
+  ref,
+) => {
   const inputElementRef = useRef<any>(null);
   const {registerField, defaultValue = '', fieldName, error} = useField(name);
   const inputValueRef = useRef<InputValueReference>({value: defaultValue});
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
+
   useEffect(() => {
     registerField<string>({
       name: fieldName,
@@ -48,4 +62,4 @@ const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
   );
 };
 
-export default Input;
+export default forwardRef(Input);
