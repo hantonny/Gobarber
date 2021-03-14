245b4e286822333cf7bfc9f4053599b04beef545
Hantonny Korrea <hantonny.20@gmail.com>
2021-03-14 18:30:35 -0400

    :rotating_light: - Aplicando TDD na prática

---
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,28 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider'
+import SendForgotPasswordEmail from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmail(fakeUsersRepository, fakeMailProvider);
+
+    await fakeUsersRepository.create({
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: '123456'
+    })
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@gmail.com',
+    })
+
+    expect(sendMail).toHaveBeenCalled();
+  })
+});
