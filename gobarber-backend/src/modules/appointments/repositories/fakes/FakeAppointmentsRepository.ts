3ab8755683fd5eb5d7d3c7756260c692cb564da6
Hantonny Korrea <hantonny.20@gmail.com>
2021-03-24 17:27:12 -0400

    :sparkles: - Filtrando agendamentos por mês

--- gobarber-backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ gobarber-backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,7 +1,8 @@
 import { uuid } from 'uuidv4';
-import {isEqual} from 'date-fns'
+import {isEqual, getMonth, getYear} from 'date-fns'
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
@@ -12,6 +13,21 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
     const findAppointment = this.appointments.find(
       appointment => isEqual(appointment.date, date)
+    );
+      return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id, 
+    month, 
+    year
+  }: IFindAllInMonthFromProviderDTO): 
+  Promise<Appointment[]> {
+
+    const findAppointment = this.appointments.filter(
+      appointment => appointment.provider_id === provider_id &&
+      getMonth(appointment.date) +1 === month &&
+      getYear(appointment.date) === year
     );
       return findAppointment;
   }
