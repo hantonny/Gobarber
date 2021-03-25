409615c58e000471927da4e57c4599f034f1758d
Hantonny Korrea <hantonny.20@gmail.com>
2021-03-25 19:48:22 -0400

    :sparkles: - Excluindo horários antigos

--- gobarber-backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns'
+import { getHours, isAfter } from 'date-fns'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -41,11 +41,17 @@ class ListProviderDayAvailability {
       length: 10
     }, (_, index) => index + hourStart
     );
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHours = appointments.find(appointment => getHours(appointment.date) === hour);
+
+
+      const compareDate = new Date(year, month - 1, day, hour)
+
       return {
         hour,
-        available: !hasAppointmentInHours
+        available: !hasAppointmentInHours && isAfter(compareDate, currentDate)
       }
     })
     return availability;
