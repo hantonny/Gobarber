e2114d6f2da670c7ec8a15daf1f8ae99efd8ef21
Hantonny Korrea <hantonny.20@gmail.com>
2021-03-27 19:31:50 -0400

    :sparkles: - Emails pelo Amazon SES

---
+++ gobarber-backend/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'hantonny.20@gmail.com',
+      name: 'Hantonny Korrea'
+    }
+  }
+} as IMailConfig;
--- gobarber-backend/src/shared/container/providers/index.ts
+++ gobarber-backend/src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailconfig from '@config/mail'
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider'
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementatios/HandlebarsMailTemplateProvider'
@@ -19,6 +21,8 @@ container.registerSingleton<IMailTemplateProvider>(
   HandlebarsMailTemplateProvider,
 )
 container.registerInstance<IMailProvider>(
-  'EtherealMailProvider',
-  container.resolve(EtherealMailProvider),
+  'MailProvider',
+  mailconfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 )
