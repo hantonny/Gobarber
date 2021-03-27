ced2493624893283084b57f747adaa07ca58dad6
Hantonny Korrea <hantonny.20@gmail.com>
2021-03-27 14:17:39 -0400

    :sparkles: - Variáveis ambiente

--- gobarber-backend/src/config/auth.ts
+++ gobarber-backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '284edf6316fe136b939add830604bd81',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -12,31 +12,31 @@ interface IRequest {
 @injectable()
 class SendForgotPasswordEmailService {
   constructor(
-  
+
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
-    
+
     @inject('MailProvider')
     private mailProvider: IMailProvider,
 
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
 
-  
-    
+
+
   ) { }
 
-  public async execute({email}: IRequest): Promise<void> {
+  public async execute({ email }: IRequest): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
-    if(!user){
+    if (!user) {
       throw new AppError('User does not exists.')
     }
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
     const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs')
-    
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -47,7 +47,7 @@ class SendForgotPasswordEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `http://localhost:3000/reset_password?token=${token}`
+          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`
         }
       }
     })
