fe7c959462ba6b95f092e47155caa2c06d4710b2
Hantonny Korrea <hantonny.20@gmail.com>
2021-03-30 21:19:23 -0400

    :sparkles: - Configurando cache

--- gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   provider_id: string;
@@ -14,6 +15,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({
@@ -22,12 +26,15 @@ class ListProviderAppointmentsService {
     month,
     day
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+    console.log(cacheData)
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       year,
       month,
       day
     })
+    // await this.cacheProvider.save('asd', 'asd')
     return appointments;
   }
 }
