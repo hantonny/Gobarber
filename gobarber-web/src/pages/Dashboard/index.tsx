f45234197f067cf2902bc8bd8a6c0477aea7a3b4
Hantonny Korrea <hantonny.20@gmail.com>
2021-05-29 19:16:50 -0400

    :sparkles: - Listagem de agendamentos

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
   Container,
@@ -8,14 +8,17 @@ import {
   Content,
   Schedule,
   Calendar,
-  NextAppointment
+  NextAppointment,
+  Section,
+  Appointment,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
+import App from '../../App';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
-  console.log(user);
   return (
     <Container>
       <Header>
@@ -54,6 +57,42 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:30
+                <div>
+                  <img src="https://avatars.githubusercontent.com/u/22649926?s=400&u=939488a2c5a3d0a93a6c79f484d8419bd1229644&v=4" alt="Hantonny Korrea" />
+                  <strong>Hantonny Korrea</strong>
+                </div>
+              </span>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:30
+                <div>
+                  <img src="https://avatars.githubusercontent.com/u/22649926?s=400&u=939488a2c5a3d0a93a6c79f484d8419bd1229644&v=4" alt="Hantonny Korrea" />
+                  <strong>Hantonny Korrea</strong>
+                </div>
+              </span>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:30
+                <div>
+                  <img src="https://avatars.githubusercontent.com/u/22649926?s=400&u=939488a2c5a3d0a93a6c79f484d8419bd1229644&v=4" alt="Hantonny Korrea" />
+                  <strong>Hantonny Korrea</strong>
+                </div>
+              </span>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
