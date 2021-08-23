2b097183014ab83fa24536edd9b9261aae627df0
Hantonny Korrea <hantonny.20@gmail.com>
2021-08-23 20:51:40 -0300

    :sparkles: - Lista de providers com serialização

--- gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICa
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -24,12 +25,13 @@ class ListProvidersService {
 
     let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`)
 
+
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users)
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users))
     }
 
     return users;
